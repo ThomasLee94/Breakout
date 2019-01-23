d0c9729412f0360bc420d33550e8a2d3cbb07302
Thomas <thomaslee9474@gmail.com>
2019-01-22 23:35:11 -0800

    [add] bricks

--- public/index.js
+++ public/index.js
@@ -7,12 +7,29 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = (canvas.width-paddleWidth)/2;
+const paddleHeight = 10;
+const paddleWidth = 75;
+const paddleX = (canvas.width-paddleWidth)/2;
 
 let rightPressed = false;
 let leftPressed = false;
+
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c=0; c < brickColumnCount; c++){
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++){
+        bricks[c][r] = {x:0, y:0}
+    }
+}
+
 
 const keyDownHandler = (e) => {
     if(e.key == "Right" || e.key == "ArrowRight"){
@@ -50,16 +67,42 @@ const drawPaddle = () => {
     ctx.closePath();
 }
 
+const drawBricks = () => {
+    for(let c = 0; c < brickColumnCount; c++){
+        for (let r = 0; r < brickRowCount; r++){
+            bricks[c][r].x = 0;
+            bricks[c][r].y = 0;
+            ctx.beginPath();
+            ctx.rect(0, 0, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks()
     drawBall();
     drawPaddle();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+
+    if(y + dy < ballRadius) {
         dy = -dy;
+    }
+
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
     }
     
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -73,7 +116,7 @@ const draw = () => {
     y += dy;
 }
 
-setInterval(draw, 10);
+let interval = setInterval(draw, 10);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
